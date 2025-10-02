f1694073a740fa5ceecab915bb6ed2b08d84ab08
ADRYEL FELIPE SAPELLI DE SOUZA <adryel_souza@estudante.sesisenai.org.br>
2025-10-02 19:24:25 -0300

    Chore: Cria a estrutura da Model Leitor

---
+++ src/main/java/Leitor.java
@@ -0,0 +1,14 @@
+public class Leitor {
+
+    // -- MÉTODOS ESTÁTICOS -- //
+
+    // LÊ O HOSTNAME
+    public static void lerHostname() {
+
+    }
+
+    // RESGATA IP A PARTIR DO HOSTNAME
+    public static void adquireIpv4() {
+
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.util.regex.*;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
