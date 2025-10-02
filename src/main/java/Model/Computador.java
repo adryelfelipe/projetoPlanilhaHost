0d07cf41f987ca8f2ae519482df336a619f04d66
ADRYEL FELIPE SAPELLI DE SOUZA <adryel_souza@estudante.sesisenai.org.br>
2025-10-02 20:35:32 -0300

    Fit: Adiciona setters e getters na model Computador

--- src/main/java/Model/Computador.java
+++ src/main/java/Model/Computador.java
@@ -13,4 +13,29 @@ public class Computador {
         this.ipv4 = ipv4;
         this.mac = mac;
     }
+
+    // -- GETTERS E SETTERS -- //
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+
+    public String getIpv4() {
+        return ipv4;
+    }
+
+    public void setIpv4(String ipv4) {
+        this.ipv4 = ipv4;
+    }
 }
